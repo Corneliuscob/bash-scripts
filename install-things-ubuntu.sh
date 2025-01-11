5a1bae316a29ee06c7e236128c7ba7894bb68e43
Ngai To Lo <NGai_to_lo@hotmail.com>
2025-01-11 11:01:27 -0800

    Update install-things-ubuntu.sh

--- install-things-ubuntu.sh
+++ install-things-ubuntu.sh
@@ -67,4 +67,11 @@ sudo usermod -aG docker $USER
 newgrp docker
 
 
+docker volume create portainer_data
+docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:2.21.5
+
+echo "portainer is now running on the local computer at prt 9443 on https"
+
+
+
 
